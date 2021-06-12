4b6d963b7051725785db8013efe194976872c822
Alonso Gutelius <alonso_gutelius@usmp.pe>
2021-06-11 19:23:57 -0500

    Modificando modelo producto

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace ejercicio.Models
@@ -9,17 +10,26 @@ namespace ejercicio.Models
         [Required]
         public string Nombre {get;set; }
         [Required]
+        [Display (Name = "Nombre del comprador")]
         public string NombreComprador {get;set; }
         [Required]
+        [Display (Name = " Celular del comprador")]
         public string CelularComprador {get;set; }
         [Required]
+        [Display (Name = " Lugar de compra")]
         public string LugarCompra {get;set; }
         [Required]
         public string Descripcion {get;set; }
         [Required]
+        public string Foto {get;set;}
+        [Required]
         public decimal Precio {get;set; }
-        
+        public DateTime FechaRegistro{get;set; }
         public Categoria Categoria {get;set; }
         public int CategoriaId{get;set; }
+        public Producto(){
+            FechaRegistro=DateTime.Now;
+
+        }
     }
 }
