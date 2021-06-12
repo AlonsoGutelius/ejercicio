31ed1ba3e1166d580fd8e471a9ed2af87da8ccda
Alonso Gutelius <alonso_gutelius@usmp.pe>
2021-06-11 19:23:47 -0500

    Creando producto controller

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using ejercicio.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ejercicio.Controllers
+{
+    public class ProductoController : Controller    
+    {
+       private readonly BuscoContext _context;
+       public ProductoController(BuscoContext context){
+           _context = context;
+       }       
+       public IActionResult Registro(){
+           ViewBag.Categorias = _context.Categorias.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+           return View();
+       }
+       [HttpPost]
+       public IActionResult Registro(Producto p){
+           if(ModelState.IsValid){
+               _context.Productos.Add(p);
+               _context.SaveChanges();
+               return RedirectToAction("Index", "Home");
+           } else{
+               return View(p);
+           }
+       }
+    }
+}
