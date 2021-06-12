bde41f98c3022cd50d5cf1fa11daf8d3603da353
Alonso Gutelius <alonso_gutelius@usmp.pe>
2021-06-11 19:22:19 -0500

    agregando base de datos al startup

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ejercicio.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,7 @@ namespace ejercicio
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<BuscoContext>(dco => dco.UseSqlServer("Server=.;database=Busco;integrated security=true"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
