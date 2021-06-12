575e46c30a5d2626ca83dcff73c2c6b082454e40
Alonso Gutelius <alonso_gutelius@usmp.pe>
2021-06-11 23:08:55 -0500

    Agregando login por roles

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using ejercicio.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +28,7 @@ namespace ejercicio
         {
             services.AddControllersWithViews();
             services.AddDbContext<BuscoContext>(dco => dco.UseSqlServer("Server=.;database=Busco;integrated security=true"));
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<BuscoContext>().AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +48,7 @@ namespace ejercicio
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
